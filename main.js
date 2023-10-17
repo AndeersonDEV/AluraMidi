3539de40a601415e2f319d5e70ecdf6be68b188e
AndeersonDEV <andeerson_sps@hotmail.com>
2023-10-17 20:18:58 -0300

    Inserindo outros sons

--- main.js
+++ main.js
@@ -1,9 +1,29 @@
-function tocaSomPom (){
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElementAudio){
+
+    document.querySelector(idElementAudio).play();
 
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
+let contador = 0;
 
+while (contador < listaDeTeclas.length) {
+
+    const tecla =listaDeTeclas[contador];
+
+    const instrumento = tecla.classList[1];
+
+   
+
+    // TEMPLATE STRING
+    const idAudio = `#som_${instrumento}`
+
+    tecla.onclick = function () {
+        tocaSom(idAudio)
+    }
+    contador = contador + 1
+
+    
+}
